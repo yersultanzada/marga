03ef15745799637d7ad8b844f3a90903c4c504f5
Yersultan Zada <yersultanzada@MacBook-Air-Yersultan.local>
2023-11-26 18:58:34 +0600

    Initial commit

---
+++ test/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+	$('input.contact-form__input_tel').mask('+7 (799) 999-99-99');
+
+	var progress = {
+		current: ( 100 / ($('.step-slide').length - 1) ),
+		total: $('.step-slide').length,
+		width: ( 100 / ($('.step-slide').length - 1) ),
+		process: doProgress
+	};
+
+	function doProgress () {}
+
+	var testSlider = $('.test-slider').bxSlider({
+		mode: 'fade',
+		infiniteLoop: false,
+		speed: 0,
+		adaptiveHeight: true,
+		adaptiveHeightSpeed: 0,
+		touchEnabled: false,
+		pager: false,
+		nextSelector: '.btn-next-container',
+		nextText: '<div class="btn-next"><span>на следующий шаг</span></div>',
+		prevSelector: '.btn-prev-container',
+		prevText: '<div class="btn-prev"><span>назад</span></div>',
+		onSliderLoad: function (currentIndex) {
+			// первоначальные стили
+			$('.main-progress__text').eq(currentIndex).addClass('main-progress__text_active');
+			$('.main-progress__extender').css('width', progress.width + '%');
+		},
+		onSlideAfter: function (slideElement, oldIndex, newIndex) {
+			// активация кнопок
+			$('.btn-next-container').removeClass('btn-next-container_active');
+			$('.btn-next').removeClass('btn-next_active btn-shine');
+
+			// изменение полосы загрузки
+			progress.current += progress.width;
+			$('.main-progress__extender').css('width', progress.current + '%');
+
+			// изменение заголовка в полосе загрузки
+			if ( $('.main-progress__text').eq(newIndex).length != 0 ) {
+				$('.main-progress__text').eq(oldIndex).removeClass('main-progress__text_active');
+				$('.main-progress__text').eq(newIndex).addClass('main-progress__text_active');
+			};
+
+			var toTopDoc = window.parent.document.querySelector('.fancybox-slide--iframe');
+
+			$(toTopDoc).animate({scrollTop: 0}, 0);
+
+		}
+	});
+
+	$('.pick-item__input').on('change', function(event) {
+		event.preventDefault();
+		$('.btn-next-container').addClass('btn-next-container_active');
+		$('.btn-next').addClass('btn-next_active btn-shine');
+	});
+
+	$('.pick-item__digit').on('focus', function(event) {
+		event.preventDefault();
+		$('.btn-next-container').addClass('btn-next-container_active');
+		$('.btn-next').addClass('btn-next_active btn-shine');
+	});
+
+});
